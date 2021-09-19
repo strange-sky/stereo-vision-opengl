c6bbaf40ad4e3b981d20e2c9837f6f00b54e7c43
Akash Bachhar <akashbachhar2001.ii@gmail.com>
2021-09-19 11:42:27 +0530

    initial commit

---
+++ build/CMakeFiles/ZED_openGL.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/akash/Desktop/opengl gpu interop/src/main.cpp" "/home/akash/Desktop/opengl gpu interop/build/CMakeFiles/ZED_openGL.dir/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/zed/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ZED_openGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_openGL.dir/src/main.o"
+  "ZED_openGL"
+  "ZED_openGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_openGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
